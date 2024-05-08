035f0c91fe9ba3818b59db0c948f91a46344f444
AryanMishra17 <151031618+AryanMishra17@users.noreply.github.com>
2024-05-08 07:20:39 +0530

    bs.cpp

--- bs.cpp
+++ bs.cpp
@@ -1,56 +1,43 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-int m;
-cout<<"size";
-cin>>m;
-int a[10];
-for(int i=0;i<m;i++)
-{
-cin>>a[i];
+    int n;
+    cout<<"Enter the size of array =>";
+    cin>>n;
+    
+    int arr[n];
+    cout<<"Enter "<<n<<" Integers in ascending order => \n";
+    for(int i=0;i<n;i++)
+        cin>>arr[i];    
+    
+    int target;
+    cout<<"Enter the number you want to search => ";
+    cin>>target;
+    
+    int start = 0, end = n-1, mid;
+    int pos= -1;
+    
+    for(int i=0;i<n;i++)
+    {
+        mid = (start+end)/2;
+        
+        if(arr[mid]==target){
+            pos = mid;
+            break;
+        }
+        else if(arr[mid]>target)
+            end = mid -1;   
+        else
+            start = mid + 1;
+    }
+    
+    if(pos==-1)
+        cout<<target<<" is not present in the array"<<endl;
+    else
+        cout<<target<<" is present in the array at position "<<pos<<endl;
+        
+    return 0;
 }
-for(int i=0;i<m;i++)
-{
-cout<<a[i]<<' ';
-}
 
-int t;
-
-cout<<"target";
-cin>>t;
-
-
-int s=0,e=n-1,pos=-1;
-while(s<=e)
-{
-mid=(s+e/m);
-if(a[m]==t)
-{
-   pos=m; //m has index position
-break;
-}
-else if(a[m]>t)
-{
-  e=m-1;
-}
-else
-{
-s=m+1;
-
-
-}
-
-
-if(pos==-1)
-{
-  cout<<"not found";
-}
-else
-{
- cout<<"found at"<<pos;
-
-}
-return 0;
-}
